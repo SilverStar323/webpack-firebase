62a3838e5d0f3fe69b291b0034b63d9475444d4c
Silverstar323 <silverstars323@gmail.com>
2024-08-11 09:24:38 -0400

    firebase setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCTcpL6ee0PYNvdmoUJe4LEMgAUHTvVSvM",
+  authDomain: "notnotion-f4c6d.firebaseapp.com",
+  projectId: "notnotion-f4c6d",
+  storageBucket: "notnotion-f4c6d.appspot.com",
+  messagingSenderId: "523478366447",
+  appId: "1:523478366447:web:b5ce62379f2b864174831a"
+};
+
+const app = initializeApp(firebaseConfig);
